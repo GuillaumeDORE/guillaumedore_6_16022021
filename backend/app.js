879cd9e3dcca0d5c3ef71ba7b611cb53a1355a1d
Guillaume DORE <guillaumedore45@gmail.com>
2021-02-18 11:36:11 +0100

    gestion erreurs CORS et install de body parser

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use((req, res, next) => {
-    console.log('Requête recu!');
-    next();
-});
 
 app.use((req, res, next) => {
-    res.status(201);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-})
+  });
 
-app.use((req, res, next) =>{
-    res.json({message: 'Votre requête a bien été reçue!'});
-    next();
-});
+app.use(bodyParser.json());
 
-app.use((req, res) =>{
-    console.log('Réponse envoyé avec succès!');
-});
+// app.use((req, res, next) => {
+//     console.log('Requête recu!');
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     res.status(201);
+//     next();
+// })
+
+// app.use((req, res, next) =>{
+//     res.json({message: 'Votre requête a bien été reçue!'});
+//     next();
+// });
+
+// app.use((req, res) =>{
+//     console.log('Réponse envoyé avec succès!');
+// });
 
 module.exports = app;
