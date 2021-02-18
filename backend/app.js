94e923cd99aeee2563786946d9cbd20ea355542f
Guillaume DORE <guillaumedore45@gmail.com>
2021-02-18 09:34:35 +0100

    Mise en place d'Express

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+app.use((req, res, next) => {
+    console.log('Requête recu!');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next) =>{
+    res.json({message: 'Votre requête a bien été reçue!'});
+    next();
+});
+
+app.use((req, res) =>{
+    console.log('Réponse envoyé avec succès!');
+});
+
+module.exports = app;
