626fd4fdedad1dda1725b6b151aa0cee5b55da42
Guillaume DORE <guillaumedore45@gmail.com>
2021-02-22 10:16:10 +0100

    Configuration MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Guillaume:sKI2jBlCNpTLnFwa@clusterp6.zfgjw.mongodb.net/ClusterP6?retryWrites=true&w=majority',
+{ useNewUrlParser: true, 
+  useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie!'))
+.catch(() => console.log('Connexion à MongoDB échoucée!'));
 
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 app.use(bodyParser.json());
 
