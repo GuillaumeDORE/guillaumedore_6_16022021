760df2f3ab25f2a5f809dfe6e61137433e3da3f9
Guillaume DORE <guillaumedore45@gmail.com>
2021-02-22 11:22:46 +0100

    Mise en place des routes delete et put de sauce

--- backend/app.js
+++ backend/app.js
@@ -31,11 +31,24 @@ app.post('/api/sauces', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
+app.put('./api/sauces/:id', ( req, res, next) => {
+  Sauce.updateOne({_id: req.params.id}, { ...req.body, _id: res.params.id})
+    .then(() => res.status(200).json({ message: 'Objet modifié!'}))
+    .catch(error => res.status(400).json({ error}));
+});
+
+app.delete('./api/sauces/:id', ( req, res, next) => {
+  Sauce.deleteOne({_id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Objet supprimé!'}))
+    .catch(error => res.status(400).json({ error}));
+});
+
 app.get('/api/sauces/:id', (req, res, next) => {
   Sauce.findOne({_id: req.params.id})
     .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(404).json({ error }));
 });
+
 app.get('/api/sauces', (req, res, next) => {
   Sauce.find()
     .then(sauces => res.status(200).json(sauces))
