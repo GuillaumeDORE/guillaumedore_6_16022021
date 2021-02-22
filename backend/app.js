01e59dbf8ac4ee203c3808e23819895ce448aeeb
Guillaume DORE <guillaumedore45@gmail.com>
2021-02-22 11:08:50 +0100

    Model sauce + implémentation route POST et GET des sauces

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const Sauce = require('.models/Sauce');
+
 mongoose.connect('mongodb+srv://Guillaume:sKI2jBlCNpTLnFwa@clusterp6.zfgjw.mongodb.net/ClusterP6?retryWrites=true&w=majority',
 { useNewUrlParser: true, 
   useUnifiedTopology: true })
@@ -19,23 +21,25 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//     console.log('Requête recu!');
-//     next();
-// });
+app.post('/api/sauces', (req, res, next) => {
+  delete req.body._id;
+  const sauce = new Sauce({
+    ...req.body
+  });
+  sauce.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
 
-// app.use((req, res, next) => {
-//     res.status(201);
-//     next();
-// })
-
-// app.use((req, res, next) =>{
-//     res.json({message: 'Votre requête a bien été reçue!'});
-//     next();
-// });
-
-// app.use((req, res) =>{
-//     console.log('Réponse envoyé avec succès!');
-// });
+app.get('/api/sauces/:id', (req, res, next) => {
+  Sauce.findOne({_id: req.params.id})
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+});
+app.get('/api/sauces', (req, res, next) => {
+  Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+});
 
 module.exports = app;
