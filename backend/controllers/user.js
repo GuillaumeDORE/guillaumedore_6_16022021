8f42e2e395d09f1c9adbe1806fe33eac74eb9a33
Guillaume DORE <guillaumedore45@gmail.com>
2021-03-11 21:13:09 +0100

    Mise en place mask email

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const passwordValidator = require('password-validator');
+const MaskData = require('maskdata');
 
 const User = require('../models/User');
 
@@ -12,14 +13,22 @@ schema
 .has().lowercase()                              // Must have lowercase letters
 .has().digits(2)                                // Must have at least 2 digits
 .has().not().spaces()                           // Should not have spaces
+.has().symbols()
 .is().not().oneOf(['Passw0rd', 'Password123', 'Azerty123']);
+
+const emailMask2Options = {
+    maskWith: "*", 
+    unmaskedStartCharactersBeforeAt: 3,
+    unmaskedEndCharactersAfterAt: 2,
+    maskAtTheRate: false
+};
 
 exports.signup = (req, res, next) => {
     if(schema.validate(req.body.password)){
         bcrypt.hash(req.body.password, 10)
         .then(hash =>{
             const user = new User({
-                email: req.body.email,
+                email: MaskData.maskEmail2(req.body.email, emailMask2Options),
                 password: hash
             });
             user.save()
@@ -28,12 +37,12 @@ exports.signup = (req, res, next) => {
         })
         .catch(error => res.status(500).json({error}));
     }else{
-        return res.status(400).json({error : 'Le mot de passe doit faire minimum 8 caractères, avoir minimum une majuscule et une minuscule et avoir minimum 2 chiffres. '});
+        return res.status(400).json({error : 'Le mot de passe doit faire minimum 8 caractères, un symbol, avoir minimum une majuscule et une minuscule et avoir minimum 2 chiffres. '});
     }
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email})
+    User.findOne({ email: MaskData.maskEmail2(req.body.email, emailMask2Options)})
     .then(user => {
         if (!user) {
             return res.status(401).json({error : 'Utilisateur non trouvé!'});
